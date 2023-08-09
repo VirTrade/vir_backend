6f6c64da86d762b8d8291e4d0398553b1a7c1b1a
PRATHAM MORE <pratham02more20@gmail.com>
2023-08-09 19:06:08 +0530

    ws error resolved

--- index.js
+++ index.js
@@ -1,104 +1,94 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(cors());
 app.use((_req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "*");
-    next();
-  });
-const server = require('http').createServer(app);
-const ws = require('ws')
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+const WebSocket = require("ws");
+const server = require("http").createServer(app);
+const ws = require("ws");
 const socketIO = require("socket.io")(server, {
-    cors: {
-      origin: "http://localhost:3000",
-      methods: ["GET", "POST"]
-    }
-  });
-const cors = require('cors')
-
-
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+const cors = require("cors");
 
 let sock;
 
 // const wss = new ws.Server({ server: server })
 // const io = socketIO(server);
 
-socketIO.on('connection', (socket) => {
-    console.log('Socket connected');
-    socket.on('sendData', (data) => {
-        console.log('Received data from frontend:', data);
-        console.log(socket.handshake.headers);
-        console.log( data.payload);
-       
+socketIO.on("connection", (socket) => {
+  console.log("Socket connected");
+  socket.on("sendData", (data) => {
+    console.log("Received data from frontend:", data);
+    console.log(socket.handshake.headers);
+    console.log(data.payload);
 
-        const WebSocket = require('ws');
+    // Create a WebSocket instance with the desired URL and headers
+    try {
+      sock = new WebSocket("ws://smartapisocket.angelone.in/smart-stream", {
+        headers: {
+          Authorization: process.env.JWT_TOKEN,
+          "x-api-key": process.env.API_KEY,
+          "x-client-code": process.env.CLIENT_KEY,
+          "x-feed-token": process.env.feed - token,
+        },
+      });
 
-        ws.on("message", (message) => {
-            const receivedBuffer = Buffer.from(message);
-            const receivedData = JSON.parse(receivedBuffer);
-            console.log("receivedData", receivedData);
-            // Create a WebSocket instance with the desired URL and headers
-            try {        
-                sock = new WebSocket('ws://smartapisocket.angelone.in/smart-stream', {
-                    headers: {
-                        'Authorization': process.env.JWT_TOKEN,
-                        'x-api-key': process.env.API_KEY,
-                        'x-client-code': process.env.CLIENT_KEY,
-                        'x-feed-token': process.env.feed-token,
-                    },
-                }); 
-    
-                sock.on('open', () => {
-                    console.log('WebSocket connection established');
-    
-                    // Send a subscribe request for LTP mode with the desired token
-                    let json_req = {                    
-                        action: 0,
-                        params: {
-                            mode: 1,
-                            tokenList: [
-                                {
-                                    exchangeType: 5,
-                                    tokens: receivedData,
-                                },
-                            ],
-                        },
-                    };
-                    sock.send(JSON.stringify(json_req));
-                });
-    
-                // Event listener for receiving messages            
-    
-                sock.on('message', (data) => {
-                    console.log('Received message:', data);
-                    ws.send(data)
-                });
-    
-                // Event listener for connection close
-                sock.on('close', (code, reason) => {
-                    console.log('WebSocket connection closed:', code, reason);
-                });
-    
-                // Event listener for connection errors
-                sock.on('error', (error) => {
-                    console.error('WebSocket connection error:', error);
-                });
-    
-                setInterval(() => {
-                    socket.send('ping')
-                }, 30000)
-            }
-            catch (e) {
-                console.log(e)
-            }
-            // ws.send("Received Credentials")
-        })
-        socket.emit('response', { message: 'Data received' });
+      sock.on("open", () => {
+        console.log("WebSocket connection established");
+
+        // Send a subscribe request for LTP mode with the desired token
+        let json_req = {
+          action: 0,
+          params: {
+            mode: 1,
+            tokenList: [
+              {
+                exchangeType: 5,
+                tokens: receivedData,
+              },
+            ],
+          },
+        };
+        sock.send(JSON.stringify(json_req));
       });
-      socket.on('disconnect', () => {
-        console.log('User disconnected');
+
+      // Event listener for receiving messages
+
+      sock.on("message", (data) => {
+        console.log("Received message:", data);
+        ws.send(data);
       });
+
+      // Event listener for connection close
+      sock.on("close", (code, reason) => {
+        console.log("WebSocket connection closed:", code, reason);
+      });
+
+      // Event listener for connection errors
+      sock.on("error", (error) => {
+        console.error("WebSocket connection error:", error);
+      });
+
+      setInterval(() => {
+        socket.send("ping");
+      }, 30000);
+    } catch (e) {
+      console.log(e);
+    }
+    // ws.send("Received Credentials")
+
+    socket.emit("response", { message: "Data received" });
+  });
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
 });
 
-
-server.listen(5000, () => console.log("Listening on Server 5000"))
+server.listen(5000, () => console.log("Listening on Server 5000"));
